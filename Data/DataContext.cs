981ad056b339ad225b5d27c4472c58bd5df7b1ca
cpineda <carlosepjorge@gmail.com>
2023-08-20 00:57:29 -0400

    Initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DatingApp_API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DatingApp_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+
+}
